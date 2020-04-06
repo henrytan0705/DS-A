5e7a4b34f0eabed2096c6b3bf84742d585116e07
HenryTan0705 <htan0705@gmail.com>
2020-04-06 16:57:01 -0400

    Create queue from scratch

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -21,11 +21,52 @@
 // -----------
 
 class Node {
-
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Queue {
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    enqueue(val) {
+        const item = new Node(val);
+
+        if (!this.front) {
+            this.front = item;
+            this.back = item;
+        } else {
+            this.back.next = item;
+            this.back = item;
+        }
+
+        return ++this.length;
+    }
+
+    dequeue() {
+        if (!this.length) return null;
+
+        --this.length;
+        const temp = this.front;
+
+        if (this.front === this.back) {
+            this.front = null;
+            this.back = null;
+        } else {
+            this.front = this.front.next;
+        }
+
+        return temp.value;
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
