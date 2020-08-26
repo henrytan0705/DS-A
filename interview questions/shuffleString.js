221763aa6dde66e8eba7bd069f1be10853c3475c
HenryTan0705 <htan0705@gmail.com>
2020-08-25 23:14:49 -0400

    Leetcode-shuffleString.js

---
+++ interview questions/shuffleString.js
@@ -0,0 +1,9 @@
+var restoreString = function(s, indices) {
+  let newString = [];
+
+  for (let i = 0; i < indices.length; i++) {
+    newString[indices[i]] = s[i];
+  }
+
+  return newString.join("");
+};
