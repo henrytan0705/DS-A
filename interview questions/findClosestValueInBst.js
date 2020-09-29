a4ce2bd8d0c92d06b5ced7b112cdb9cc80094d21
HenryTan0705 <htan0705@gmail.com>
2020-09-28 23:20:58 -0400

    find closest value is bst - js

---
+++ interview questions/findClosestValueInBst.js
@@ -0,0 +1,27 @@
+function findClosestValueInBst(tree, target) {
+  return findClosestHelper(tree, target, tree.value);
+}
+
+function findClosestHelper(tree, target, closest) {
+	if (tree === null) return closest;
+	
+	if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+		closest = tree.value;
+	}
+	
+	if (target < tree.value) {
+		return findClosestHelper(tree.left, target, closest);
+	} else if (target > tree.value) {
+		return findClosestHelper(tree.right, target, closest);
+	} else {
+		return closest;
+	}
+}
+// This is the class of the input tree. Do not edit.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
