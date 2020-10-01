56263c568c93d201cbf80df7bf571d127db5fc0f
HenryTan0705 <htan0705@gmail.com>
2020-10-01 00:08:54 -0400

    BST Traversal - js

---
+++ interview questions/bstTraversal.js
@@ -0,0 +1,32 @@
+// left child - root - right child
+function inOrderTraverse(tree, array) {
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+
+  return array;
+}
+
+// root - left child - right child
+function preOrderTraverse(tree, array) {
+  if (tree !== null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+  }
+
+  return array;
+}
+
+// left child - right child - root
+function postOrderTraverse(tree, array) {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+  }
+
+  return array;
+}
