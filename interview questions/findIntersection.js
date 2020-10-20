1a199ce4799eb09795c1e26385f352bc0576380b
HenryTan0705 <htan0705@gmail.com>
2020-10-20 18:47:06 -0400

    Find Intersection -js

---
+++ interview questions/findIntersection.js
@@ -0,0 +1,14 @@
+// Input = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+// Output = "1,3,13"
+
+function FindIntersection(strArr) {
+  const results = [];
+  const arr1 = strArr[0].split(", ");
+  const arr2 = strArr[1].split(", ");
+
+  for (const num1 of arr1) {
+    if (arr2.includes(num1)) results.push(num1);
+  }
+
+  return results.length ? results.join(",") : false;
+}
